f70aa3dd9aee0c20f528920943270d1e1c6a65df
otonielvieira <ottobuzios@gmail.com>
2022-01-20 07:33:07 -0300

    First commit, My First Django Project

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+import datetime
+
+date = datetime.date
+
+def home(request):
+    html ='''<html>
+    <h1>First Django Project</h1>
+    <br>
+    <h2>Exploring the Power od Python With Django!!</h2>
+    <br>
+    <h3>This Project is only .....</h3>
+    </html>'''
+    return HttpResponse(html)
