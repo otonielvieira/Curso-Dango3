a27220369de7a757fdd2b5c2671a8a1edecbf759
otonielvieira <ottobuzios@gmail.com>
2022-01-21 18:13:57 -0300

    Estágio do projeto: trazendo registro do banco de dados e renderizando no template html

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Posts
 
 # Create your views here.
+list_posts = Posts.objects.all()
+lista = ['Django', 'Python', 'Flask', 'KIvy', 'Html', "Css", 'Php', 'Java', 'kotin']
+data = {'name': 'Curso de Django, Renderizando Templates', 'lista_tecnologias': lista, 'result': list_posts}
+
 def site(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', data)
