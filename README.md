4d60fda4183705563389b320bb0960c76a2ee678
otonielvieira <ottobuzios@gmail.com>
2022-01-21 07:50:18 -0300

    Organizando o código, corrigindo a url.py que estava errada. Criando o primeiro template html

---
+++ README.md
@@ -0,0 +1,4 @@
+# Curso-Dango3
+First files from Django3
+
+#This is a simple initial file from django course
--- blog/website/views.py
+++ blog/website/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def site(request):
-    return HttpResponse('<h1>Hello Main Pagem form main Dajango App<h1>')
+    return render(request, 'index.html')
