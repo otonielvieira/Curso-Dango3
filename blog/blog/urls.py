6c7d5afe07afdd2c1b4c82922982a84136d6d27f
otonielvieira <ottobuzios@gmail.com>
2022-01-29 07:19:06 -0300

    Corrigindo erro de path imagem

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('home', home),
     path('site',include('website.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
